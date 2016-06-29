a3b3b8665c367038e496b7da65743df1bb5bdedf
Jeremy Wells <jemmyw@gmail.com>
2016-06-29 17:10:43 +1200

    feat(Deploy): Add cache purge to production deploy

--- bin/purge-cache.sh
+++ bin/purge-cache.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-CLOUDFLARE_ZONE=`curl -X GET -H "X-Auth-Email: ${CLOUDFLARE_AUTH_EMAIL}" -H "X-Auth-Key: ${CLOUDFLARE_AUTH_KEY}" -H "Content-Type: application/json" "https://api.cloudflare.com/client/v4/zones?name=sparks.network" | jq -r ".result[0].id"`
+CLOUDFLARE_DOMAIN="$1"
+
+CLOUDFLARE_ZONE=`curl -X GET -H "X-Auth-Email: ${CLOUDFLARE_AUTH_EMAIL}" -H "X-Auth-Key: ${CLOUDFLARE_AUTH_KEY}" -H "Content-Type: application/json" "https://api.cloudflare.com/client/v4/zones?name=${CLOUDFLARE_DOMAIN}" | jq -r ".result[0].id"`
 
 echo "Purging ${CLOUDFLARE_ZONE}"
 
