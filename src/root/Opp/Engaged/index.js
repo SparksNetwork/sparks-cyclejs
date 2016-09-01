ba5b3ad6a64485350475647204d6a4fd78d58656
sd <sdebaun74@gmail.com>
2016-09-01 05:54:41 -0700

    feat(opps): add 'All' tab and change nav labels for opp

--- src/root/Opp/Engaged/index.js
+++ src/root/Opp/Engaged/index.js
@@ -6,37 +6,23 @@ import {TabbedPage} from 'components/ui'
 
 import EngagedList from 'components/EngagedList'
 
-const statuses = ['applied', 'ok', 'confirmed', 'never']
+const statuses = ['applied', 'ok', 'confirmed', 'never', 'engagements']
 const labels = {
   applied: 'Applied',
   ok: 'Approved',
   confirmed: 'Confirmed',
   never: 'Rejected',
+  engagements: 'All',
 }
 const paths = {applied: '/'}
 
 const _TabMaker = sources => ({
-// <<<<<<< HEAD
-//   tabs$: combineLatest(
-//     sources.applied$,
-//     sources.priority$,
-//     sources.ok$,
-//     sources.never$,
-//     (ap,pr,ok,nv) => [
-//       {path: '/', label: `${ap.length} Applied`},
-//       pr.length > 0 && {path: '/priority', label: `${pr.length} Priority`},
-//       ok.length > 0 && {path: '/ok', label: `${ok.length} Accepted`},
-//       nv.length > 0 && {path: '/never', label: `${nv.length} Rejected`},
-//     ].filter(x => !!x)
-//   ),
-// =======
   tabs$: combineLatest(...statuses.map(status =>
     sources[`${status}$`].map(prop('length')).map(count => ({
       path: paths[status] || `/${status}`,
       label: `${count} ${labels[status]}`,
     }))
   )),
-// >>>>>>> feature/unified-admin-profiles
 })
 
 const StatusList = status => sources => EngagedList({...sources,
--- src/root/Opp/OppNav.js
+++ src/root/Opp/OppNav.js
@@ -25,22 +25,23 @@ const _Manage = sources => ListItemNavigating({...sources,
 
 const _Confirmed = sources => ListItemNavigating({...sources,
   title$: sources.confirmed$.map(c => `${c.length || 0} Confirmed`),
+  subtitle$: sources.applied$.map(c => `${c.length || 0} Need Approval`),
   iconName$: just('people'),
   path$: just('/engaged/confirmed'),
 })
 
-const _Engaged = sources => ListItemNavigating({...sources,
-  title$: sources.applied$.map(c => `${c.length || 0} Applied`),
-  iconName$: just('event_available'),
-  path$: just('/engaged'),
-})
+// const _Engaged = sources => ListItemNavigating({...sources,
+//   title$: sources.applied$.map(c => `${c.length || 0} Applied`),
+//   iconName$: just('event_available'),
+//   path$: just('/engaged'),
+// })
 
 const _List = sources => {
   const childs = [
     isolate(_Glance,'glance')(sources),
     isolate(_Manage,'manage')(sources),
     isolate(_Confirmed,'confirmed')(sources),
-    isolate(_Engaged,'enaged')(sources),
+    // isolate(_Engaged,'enaged')(sources),
   ]
 
   return {
